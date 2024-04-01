ab07e6100f784db638329b1fc7549005ee74222c
Julian Vitor <julian.vtr@gmail.com>
2024-04-01 03:40:58 -0300

    tipos

---
+++ basico/tipos.js
@@ -0,0 +1,12 @@
+//javascript tem tipagem fraca. 
+let numero = 10;
+let string = "hello world";
+let booleano = true;
+let array = [1,2,3];
+let objeto = {chave: "valor", valor: 5};
+
+console.log(typeof numero); // Saída: "number"
+console.log(typeof texto); // Saída: "string"
+console.log(typeof booleano); // Saída: "boolean"
+console.log(typeof arrayDeNumeros); // Saída: "object"
+console.log(typeof objeto); // Saída: "object"
